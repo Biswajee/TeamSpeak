3d51245e7fe8d7c5fc03e6ff65bf70e02c146be8
Biswajee <roy.biswajeet161@gmail.com>
2018-09-10 23:32:51 +0530

    Add modules...

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const mongo = require('mongodb').MongoClient;
+
+//Connect to mongo...
+mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
+  if(err){
+      throw err;
+  }
+});
+
+console.log('mongoDB Connected...');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,9 +24,46 @@ io.on('connection', function(socket){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+  let chats = db.collection('codechef-chats');
+
+  // Create function to send status
+        sendStatus = function(s){
+            io.emit('status', s);
+        }
+
+
+        // Get chats from mongo collection
+                chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+                    if(err){
+                        throw err;
+                    }
+
+                    // Emit the messages
+                    socket.emit('output', res);
+                });
+
+                // Handle input events
+                socket.on('input', function(data){
+                    let name = data.name;
+                    let message = data.message;
+
+                    // Check for name and message
+                    if(name == '' || message == ''){
+                        // Send error status
+                        sendStatus('Please enter a name and message');
+                    } else {
+                        // Insert message
+                        chat.insert({name: name, message: message}, function(){
+                            io.emit('output', [data]);
+
+                            // Send status object
+                            sendStatus({
+                                message: 'Message sent',
+                                clear: true
+                            });
+                        });
+                    }
+                });
 });
 
 http.listen(3000, function(){
